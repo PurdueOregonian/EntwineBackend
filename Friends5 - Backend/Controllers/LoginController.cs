fd92fc26101292fe685f1cd24084efacc70a4af0
PurdueOregonian <williamliang451@gmail.com>
2023-11-15 21:49:47 -0800

    start login

--- Friends5 - Backend/Controllers/LoginController.cs
+++ Friends5 - Backend/Controllers/LoginController.cs
@@ -1,6 +1,10 @@
 using Friends5___Backend.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Friends5___Backend.Controllers
 {
@@ -8,11 +12,41 @@ namespace Friends5___Backend.Controllers
     [Route("[controller]")]
     public class LoginController : ControllerBase
     {
+        IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
         [AllowAnonymous]
         [HttpPost]
         public IActionResult Login([FromBody] LoginInfo info)
         {
-            
+            var token = GenerateJwtToken(info);
+
+            return Ok(new { token });
+        }
+
+        private string GenerateJwtToken(LoginInfo info)
+        {
+            var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, info.Username)
+        };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetValue<string>("Jwt:Key")!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddMinutes(30); // Token expiration time
+
+            var token = new JwtSecurityToken(
+                _config.GetValue<string>("Jwt:Issuer"),
+                _config.GetValue<string>("Jwt:Audience"),
+                claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
---
+++ Friends5 - Backend/Services/AuthService.cs
@@ -0,0 +1,13 @@
+﻿using Friends5___Backend.Authentication;
+
+namespace Friends5___Backend.Services
+{
+    public class AuthService
+    {
+        public async Task<bool> Login(LoginInfo loginInfo)
+        {
+            //temp
+            return true;
+        }
+    }
+}
