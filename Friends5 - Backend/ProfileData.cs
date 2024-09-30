6f4910da705044c02125ceaff1f66f627c54f1ef
PurdueOregonian <williamliang451@gmail.com>
2024-09-29 17:18:48 -0700

    ProfileData unique index

--- Friends5 - Backend/ProfileData.cs
+++ Friends5 - Backend/ProfileData.cs
@@ -1,7 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Friends5___Backend
 {
+    [Index(nameof(Username), IsUnique = true)]
     public class ProfileData
     {
         [Key]
