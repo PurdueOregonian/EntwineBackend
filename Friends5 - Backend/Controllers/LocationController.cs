7da3e3740b4929a33e8597183b66ff6ad2cf7790
William Liang <williamliang451@gmail.com>
2024-12-16 20:53:43 -0800

    return city, country, and state

--- Friends5 - Backend/Controllers/LocationController.cs
+++ Friends5 - Backend/Controllers/LocationController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Npgsql;
-using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace Friends5___Backend.Controllers
 {
@@ -33,10 +32,48 @@ namespace Friends5___Backend.Controllers
             var httpClient = _httpClientFactory.CreateClient();
             var locationResponse = await httpClient.GetAsync(
                 $"https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key=AIzaSyCdvn48IcClIf3q94R7-8V3JJ-BH5zrBxo");
-            var locationString = await locationResponse.Content.ReadAsStringAsync();
-            var location = JsonSerializer.Deserialize<object>(locationString, DefaultJsonOptions.Instance);
 
-            return Ok();
+            if (locationResponse.IsSuccessStatusCode)
+            {
+                string json = await locationResponse.Content.ReadAsStringAsync();
+                JsonNode? data = JsonNode.Parse(json);
+
+                foreach (var result in data["results"].AsArray())
+                {
+                    string? city = null;
+                    string? country = null;
+                    string? state = null;
+
+                    foreach (var component in result["address_components"].AsArray())
+                    {
+                        var componentTypes = component?["types"]?.AsArray();
+                        if (componentTypes != null)
+                        {
+                            if (componentTypes.ToString().Contains("locality"))
+                            {
+                                city = component?["long_name"]?.ToString();
+                            }
+                            else if (componentTypes.ToString().Contains("country"))
+                            {
+                                country = component?["long_name"]?.ToString();
+                            }
+                            else if (componentTypes.ToString().Contains("administrative_area_level_1"))
+                            {
+                                state = component?["long_name"]?.ToString();
+                            }
+                        }
+                    }
+
+                    if (city != null && country != null)
+                    {
+                        return Ok(new { City = city, Country = country, State = state });
+                    }
+                }
+
+                return NotFound("City not found");
+            }
+
+            return StatusCode(500, new { Message = "Failed to fetch geocoding data." });
         }
     }
 }
