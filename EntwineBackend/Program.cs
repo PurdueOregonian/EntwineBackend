0c9521086dab43a153f33912e40dda0a1a785be8
William Liang <williamliang451@gmail.com>
2025-01-10 20:31:56 -0800

    fix SignalR connection so that WebSockets work

--- EntwineBackend/Program.cs
+++ EntwineBackend/Program.cs
@@ -62,6 +62,22 @@ builder.Services.AddAuthentication(options =>
             ValidAudience = builder.Configuration.GetValue<string>("Jwt:Audience"),
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("Jwt:Key")!))
         };
+
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+
+                var path = context.HttpContext.Request.Path;
+                if (!string.IsNullOrEmpty(accessToken) &&
+                    path.StartsWithSegments("/chatHub"))
+                {
+                    context.Token = accessToken;
+                }
+                return Task.CompletedTask;
+            }
+        };
     });
 
 builder.Services.AddAuthorization(options =>
