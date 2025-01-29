4edef488c0e21b570bfc252c28e39e46992f770f
William Liang <williamliang451@gmail.com>
2025-01-28 19:08:05 -0800

    change default interests to constant

---
+++ EntwineBackend/Constants.cs
@@ -0,0 +1,35 @@
+﻿using EntwineBackend.DbItems;
+
+namespace Friends5___Backend
+{
+    public static class Constants
+    {
+        public static readonly InterestCategory[] InterestCategories = [
+            new InterestCategory { Id = 1, Name = "Creativity" },
+                new InterestCategory { Id = 2, Name = "Nature" },
+                new InterestCategory { Id = 3, Name = "Technology" },
+                new InterestCategory { Id = 4, Name = "Fitness & Sports" },
+                new InterestCategory { Id = 5, Name = "Travel" },
+                new InterestCategory { Id = 6, Name = "Music" }
+            ];
+
+        public static readonly Interest[] Interests = [
+            new Interest { Id = 1, Categories = [1], Name = "Digital Art" },
+                new Interest { Id = 2, Categories = [1], Name = "Photography" },
+                new Interest { Id = 3, Categories = [1], Name = "Painting" },
+                new Interest { Id = 4, Categories = [1], Name = "Knitting" },
+                new Interest { Id = 5, Categories = [1, 6], Name = "Music Composition" },
+                new Interest { Id = 6, Categories = [1, 6], Name = "Cooking" },
+                new Interest { Id = 7, Categories = [1, 6], Name = "Woodworking" },
+                new Interest { Id = 8, Categories = [2, 4], Name = "Hiking" },
+                new Interest { Id = 9, Categories = [2], Name = "Gardening" },
+                new Interest { Id = 10, Categories = [1, 3], Name = "Programming" },
+                new Interest { Id = 11, Categories = [1, 3], Name = "Hardware Assembly" },
+                new Interest { Id = 12, Categories = [4], Name = "Basketball" },
+                new Interest { Id = 13, Categories = [4], Name = "Soccer" },
+                new Interest { Id = 14, Categories = [5], Name = "Backpacking" },
+                new Interest { Id = 15, Categories = [6], Name = "Jazz" },
+                new Interest { Id = 16, Categories = [6], Name = "Pop" }
+            ];
+    }
+}
--- EntwineBackend/DbContext/FriendsDbContext.cs
+++ EntwineBackend/DbContext/FriendsDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using EntwineBackend.DbItems;
 using EntwineBackend.UserId;
+using Friends5___Backend;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -34,33 +35,9 @@ namespace EntwineBackend.DbContext
                 entity.HasKey(l => l.Id);
             });
 
-            modelBuilder.Entity<InterestCategory>().HasData(
-                new InterestCategory { Id = 1, Name = "Creativity" },
-                new InterestCategory { Id = 2, Name = "Nature" },
-                new InterestCategory { Id = 3, Name = "Technology" },
-                new InterestCategory { Id = 4, Name = "Fitness & Sports" },
-                new InterestCategory { Id = 5, Name = "Travel" },
-                new InterestCategory { Id = 6, Name = "Music" }
-            );
+            modelBuilder.Entity<InterestCategory>().HasData(Constants.InterestCategories);
 
-            modelBuilder.Entity<Interest>().HasData(
-                new Interest { Id = 1, Categories = [1], Name = "Digital Art" },
-                new Interest { Id = 2, Categories = [1], Name = "Photography" },
-                new Interest { Id = 3, Categories = [1], Name = "Painting" },
-                new Interest { Id = 4, Categories = [1], Name = "Knitting" },
-                new Interest { Id = 5, Categories = [1, 6], Name = "Music Composition" },
-                new Interest { Id = 6, Categories = [1, 6], Name = "Cooking" },
-                new Interest { Id = 7, Categories = [1, 6], Name = "Woodworking" },
-                new Interest { Id = 8, Categories = [2, 4], Name = "Hiking" },
-                new Interest { Id = 9, Categories = [2], Name = "Gardening" },
-                new Interest { Id = 10, Categories = [1, 3], Name = "Programming" },
-                new Interest { Id = 11, Categories = [1, 3], Name = "Hardware Assembly" },
-                new Interest { Id = 12, Categories = [4], Name = "Basketball" },
-                new Interest { Id = 13, Categories = [4], Name = "Soccer" },
-                new Interest { Id = 14, Categories = [5], Name = "Backpacking" },
-                new Interest { Id = 15, Categories = [6], Name = "Jazz" },
-                new Interest { Id = 16, Categories = [6], Name = "Pop" }
-            );
+            modelBuilder.Entity<Interest>().HasData(Constants.Interests);
         }
     }
 }
