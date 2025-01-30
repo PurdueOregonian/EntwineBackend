58ab29f25736d74b1fa38269d9b8359962ec8df4
William Liang <williamliang451@gmail.com>
2025-01-29 23:04:41 -0800

    do the database migration

--- EntwineBackend/Migrations/20250130070316_CommunityChats.Designer.cs
+++ EntwineBackend/Migrations/20250130070316_CommunityChats.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using EntwineBackend.DbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Friends5___Backend.Migrations
 {
     [DbContext(typeof(FriendsDbContext))]
-    partial class FriendsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250130070316_CommunityChats")]
+    partial class CommunityChats
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EntwineBackend/Migrations/20250130070316_CommunityChats.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Friends5___Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommunityChats : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CommunityChatMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ChatId = table.Column<int>(type: "integer", nullable: false),
+                    SenderId = table.Column<int>(type: "integer", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    TimeSent = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommunityChatMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CommunityChats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Community = table.Column<int>(type: "integer", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommunityChats", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Categories",
+                value: new List<int> { 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Categories",
+                value: new List<int> { 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Categories",
+                value: new List<int> { 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Categories",
+                value: new List<int> { 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Categories",
+                value: new List<int> { 1, 6 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Categories",
+                value: new List<int> { 1, 6 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Categories",
+                value: new List<int> { 1, 6 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Categories",
+                value: new List<int> { 2, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Categories",
+                value: new List<int> { 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Categories",
+                value: new List<int> { 1, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Categories",
+                value: new List<int> { 1, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Categories",
+                value: new List<int> { 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "Categories",
+                value: new List<int> { 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "Categories",
+                value: new List<int> { 5 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "Categories",
+                value: new List<int> { 6 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Categories",
+                value: new List<int> { 6 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommunityChatMessages_ChatId",
+                table: "CommunityChatMessages",
+                column: "ChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommunityChats_Id",
+                table: "CommunityChats",
+                column: "Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommunityChatMessages");
+
+            migrationBuilder.DropTable(
+                name: "CommunityChats");
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Categories",
+                value: new List<int> { 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Categories",
+                value: new List<int> { 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Categories",
+                value: new List<int> { 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Categories",
+                value: new List<int> { 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Categories",
+                value: new List<int> { 1, 6 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Categories",
+                value: new List<int> { 1, 6 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Categories",
+                value: new List<int> { 1, 6 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Categories",
+                value: new List<int> { 2, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Categories",
+                value: new List<int> { 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Categories",
+                value: new List<int> { 1, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Categories",
+                value: new List<int> { 1, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Categories",
+                value: new List<int> { 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "Categories",
+                value: new List<int> { 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "Categories",
+                value: new List<int> { 5 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "Categories",
+                value: new List<int> { 6 });
+
+            migrationBuilder.UpdateData(
+                table: "Interests",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Categories",
+                value: new List<int> { 6 });
+        }
+    }
+}
